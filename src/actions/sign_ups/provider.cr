c7bac48f9ec36063cbe90da75f6b3cd9ccfa03f1
Rémy Marronnier <rmarronnier@gmail.com>
2021-05-13 15:30:12 +0200

    WIP

---
+++ src/actions/sign_ups/provider.cr
@@ -0,0 +1,16 @@
+class SignUps::Provider < BrowserAction
+  include Auth::RedirectSignedInUsers
+
+  post "/sign_up_provider" do
+    SignUpUserViaProvider.create(params) do |operation, user|
+      if user
+        flash.info = "Thanks for signing up."
+        sign_in(user)
+        redirect Me::Show
+      else
+        flash.info = "Couldn't sign you up."
+        redirect SignUps::New
+      end
+    end
+  end
+end
--- src/operations/confirm_sign_up.cr
+++ src/operations/confirm_sign_up.cr
@@ -1,7 +1,7 @@
 class ConfirmSignUp < User::SaveOperation
   param_key :user
 
-  include CustomerInit
+  # include CustomerInit
 
   permit_columns confirmed_at
 end
