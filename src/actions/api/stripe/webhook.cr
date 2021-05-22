8a563c578acb5ea2ccf866e14fd1e35f5965f82a
Rémy Marronnier <rmarronnier@gmail.com>
2021-05-22 19:16:46 +0200

    test wzebhook request.headers

--- src/actions/api/stripe/webhook.cr
+++ src/actions/api/stripe/webhook.cr
@@ -4,24 +4,24 @@ class StripeEvents::Webhook < ApiAction
   post "/stripe/webhook" do
     # You can use webhooks to receive information about asynchronous payment events.
     # For more about our webhook events check out https://stripe.com/docs/webhooks.
-    webhook_secret = Application.settings.stripe_webhook_secret
-    head 500 if webhook_secret.nil?
-    payload = request.body.to_s
-    # Retrieve the event by verifying the signature using the raw body and secret if webhook signing is configured.
     json(request.headers)
-    sig_header = request.headers["Stripe-Signature"]
-    # event = nil
-    begin
-      event = Stripe::Webhook.construct_event(payload, sig_header, webhook_secret.not_nil!)
-    rescue SignatureVerificationError
-      # Invalid signature
-      puts "⚠️  Webhook signature verification failed."
-      head 400
-    end
-    # Get the type of webhook event sent - used to check the status of PaymentIntents.
-    event_type = event.not_nil!.type
-    # data_object = data["object"]
-    puts "🔔  Payment succeeded!" if event_type == "checkout.session.completed"
-    json({status: "success"})
+    # webhook_secret = Application.settings.stripe_webhook_secret
+    # head 500 if webhook_secret.nil?
+    # payload = request.body.to_s
+    # # Retrieve the event by verifying the signature using the raw body and secret if webhook signing is configured.
+    # sig_header = request.headers["Stripe-Signature"]
+    # # event = nil
+    # begin
+    #   event = Stripe::Webhook.construct_event(payload, sig_header, webhook_secret.not_nil!)
+    # rescue SignatureVerificationError
+    #   # Invalid signature
+    #   puts "⚠️  Webhook signature verification failed."
+    #   head 400
+    # end
+    # # Get the type of webhook event sent - used to check the status of PaymentIntents.
+    # event_type = event.not_nil!.type
+    # # data_object = data["object"]
+    # puts "🔔  Payment succeeded!" if event_type == "checkout.session.completed"
+    # json({status: "success"})
   end
 end
