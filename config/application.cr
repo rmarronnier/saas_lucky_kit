5c266bc598bbbadd36f15f86c125185eeaa2f83a
Rémy Marronnier <rmarronnier@gmail.com>
2021-05-25 19:48:14 +0200

    More ENV config

--- config/application.cr
+++ config/application.cr
@@ -7,8 +7,8 @@ class Application
 end
 
 Application.configure do |settings|
-  settings.name = "SaaS Kit"
-  settings.support_email = Carbon::Address.new(settings.name, "no-reply@saas_kit.com")
+  settings.name = ENV["APP_NAME"]
+  settings.support_email = Carbon::Address.new(settings.name, "no-reply@#{ENV["APP_DOMAIN"]}")
   settings.stripe_webhook_secret = ENV["STRIPE_WEBHOOK_SECRET"]
   puts "sd4658f4ds56f44sdf564sd56f4s6d54fsd65f4s65df4sd65f4sdf4545dfs45d6sfsdf456" if ENV["STRIPE_WEBHOOK_SECRET"].size < 10
 end
